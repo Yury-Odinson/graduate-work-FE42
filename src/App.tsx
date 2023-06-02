fa3f761226daa3fc657248c609a1f6eb58f7ce3b
yury-odinson <yury.odinson@gmail.com>
2023-06-02 14:10:56 +0300

    feat(signin-window): added react-router-dom structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { SignInWrapper } from './components/SignInWrapper';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <SignInWrapper />
+      <BrowserRouter>
+        <SignInWrapper />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -9,7 +9,7 @@ export const SignIn = () => {
                 <input type="text" className="form__input" placeholder="Your email"></input>
                 <span className="form-elem__span">Password</span>
                 <input type="text" className="form__input" placeholder="Your password"></input>
-                <a href="./"><span className="form-elem__spanPass">Forgot password?</span></a>
+                <a href="./" className="form-elem__spanPass"><span>Forgot password?</span></a>
                 <button className="form-elem__button">Sign in</button>
                 <span className="form-elem__footerSpan">Don't have account? <a href="./">Sign Up</a></span>
             </div>
