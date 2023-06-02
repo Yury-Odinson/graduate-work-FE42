bde77227cc8cd58847fa1916b0ed78cd7ba6b753
yury-odinson <yury.odinson@gmail.com>
2023-06-02 15:14:16 +0300

    feat(resetpassword-window): structure recycling components

---
+++ src/components/MainTmp.tsx
@@ -0,0 +1,12 @@
+// this file is tmp!
+
+import { Link } from "react-router-dom"
+
+export const Main = () => {
+    return (
+        <>
+            <div> main tmp window</div>
+            <Link to={"/login"}><button>SDVSG</button></Link>
+        </>
+    )
+}
