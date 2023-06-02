65b7e8938b7392ce37c2a1643486077b57bbf6a8
yury-odinson <yury.odinson@gmail.com>
2023-06-02 13:23:21 +0300

    feat(signin-window): added window for login users

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,12 @@
+import { SignInWrapper } from "./SignInWrapper";
+import "../styles/wrapper.css"
+
+export const Wrapper = () => {
+    return (
+        <>
+            <div className="wrapper">
+                <SignInWrapper />
+            </div>
+        </>
+    )
+}
