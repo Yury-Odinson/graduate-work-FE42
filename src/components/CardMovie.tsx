55a807b97b32c30df82ceefefa5b56b8fbe84b6b
yury-odinson <yury.odinson@gmail.com>
2023-06-05 14:58:49 +0300

    feat(main-page): rating in a card movie

--- src/components/CardMovie.tsx
+++ src/components/CardMovie.tsx
@@ -3,7 +3,7 @@ export const CardMovie = () => {
         <>
             <div className="movie-item">
                 <img className="movie-item__image" src="https://basecourse.ru/css/images/sizeimage.jpg" alt="asdf" />
-                <div className="movie-item__rating"></div>
+                <div className="movie-item__rating">7.6</div>
                 <span className="movie-item__name">Star Wars: The Rise of Skywalker</span>
                 <ul className="movie-item__genres">
                     <li className="movie-item__genre">Action</li>
