c77f1d03d62982c9f1ba4a0f4f355738d3b9ff8e
yury-odinson <yury.odinson@gmail.com>
2023-06-06 12:00:30 +0300

    feat(authorization): added check password

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,19 +1,30 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 export const SignUp = () => {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmP, setConfirmP] = useState("")
+
+    const getWrongPassword = (pass: string, confirm: string) => pass !== confirm && confirm !== "" && <p className="form-elem__wrongPassword">check password</p>
+    const checkPass = (pass: string, confirm: string) => pass === confirm && pass
+
     return (
         <>
             <div className="form">
                 <p className="form__title">Sign Up</p>
                 <span className="form-elem__span">Name</span>
-                <input type="text" className="form__input" placeholder="Your name"></input>
+                <input type="text" className="form__input" placeholder="Your name" onChange={(e) => { setName(e.target.value) }}></input>
                 <span className="form-elem__span">Email</span>
-                <input type="text" className="form__input" placeholder="Your email"></input>
+                <input type="text" className="form__input" placeholder="Your email" onChange={(e) => { setEmail(e.target.value) }}></input>
                 <span className="form-elem__span">Password</span>
-                <input type="text" className="form__input" placeholder="Your password"></input>
+                <input type="password" className="form__input" placeholder="Your password" onChange={(e) => { setPassword(e.target.value) }}></input>
                 <span className="form-elem__span">Confirm password</span>
-                <input type="text" className="form__input" placeholder="Confirm password"></input>
-                <button className="form-elem__button">Sign up</button>
+                <input type="password" className="form__input" placeholder="Confirm password" onChange={(e) => { setConfirmP(e.target.value) }}></input>
+                {getWrongPassword(password, confirmP)}
+                <button className="form-elem__button" onClick={() => checkPass(password, confirmP)}>Sign up</button>
                 <span className="form-elem__footerSpan">Already have an account? &nbsp;
                     <Link to={"/login"}>
                         Sign in!
---
+++ src/tools/users.ts
@@ -0,0 +1,9 @@
+type User = {
+    name: string | null;
+    email: string | null;
+    password: string | null;
+}
+
+export const onRegUser = async ({ name, email, password }: User) => {
+    
+}
