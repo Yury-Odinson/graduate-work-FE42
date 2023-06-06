416e2d3ca9d54f8486eabea60b6f2243d00d0e08
yury-odinson <yury.odinson@gmail.com>
2023-06-06 15:19:56 +0300

    feat(authorization): added registration user

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,30 +1,48 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import { User, regUser } from "../tools/users"
 
 export const SignUp = () => {
 
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmP, setConfirmP] = useState("")
+    const [userName, setUserName] = useState("")
+    const [userEmail, setUserEmail] = useState("")
+    const [userPassword, setUserPassword] = useState("")
+    const [confirmUserPassword, setConfirmUserPassword] = useState("")
+    const [showPass, setShowPass] = useState("password")
+    const [showConfirmPass, setShowConfirmPass] = useState("password")
 
     const getWrongPassword = (pass: string, confirm: string) => pass !== confirm && confirm !== "" && <p className="form-elem__wrongPassword">check password</p>
     const checkPass = (pass: string, confirm: string) => pass === confirm && pass
+
+    const addUser = (inputName: string, inputEmail: string, inputPassword: string) => {
+        const newUser: User = {
+            username: inputName,
+            email: inputEmail,
+            password: inputPassword
+        }
+        return regUser(newUser)
+    }
 
     return (
         <>
             <div className="form">
                 <p className="form__title">Sign Up</p>
                 <span className="form-elem__span">Name</span>
-                <input type="text" className="form__input" placeholder="Your name" onChange={(e) => { setName(e.target.value) }}></input>
+                <input type="text" className="form__input" placeholder="Your name" onChange={(e) => { setUserName(e.target.value) }}></input>
                 <span className="form-elem__span">Email</span>
-                <input type="text" className="form__input" placeholder="Your email" onChange={(e) => { setEmail(e.target.value) }}></input>
+                <input type="text" className="form__input" placeholder="Your email" onChange={(e) => { setUserEmail(e.target.value) }}></input>
                 <span className="form-elem__span">Password</span>
-                <input type="password" className="form__input" placeholder="Your password" onChange={(e) => { setPassword(e.target.value) }}></input>
+                <label className="form-elem__pass">
+                    <input type={showPass} className="form__input" placeholder="Your password" onChange={(e) => { setUserPassword(e.target.value) }}></input>
+                    <img className="form-elem__showpass" src="../images/pass_show.png" onClick={() => setShowPass("text")} />
+                </label>
                 <span className="form-elem__span">Confirm password</span>
-                <input type="password" className="form__input" placeholder="Confirm password" onChange={(e) => { setConfirmP(e.target.value) }}></input>
-                {getWrongPassword(password, confirmP)}
-                <button className="form-elem__button" onClick={() => checkPass(password, confirmP)}>Sign up</button>
+                <label className="form-elem__pass">
+                    <input type={showConfirmPass} className="form__input" placeholder="Confirm password" onChange={(e) => { setConfirmUserPassword(e.target.value) }}></input>
+                    <img className="form-elem__showpass" src="../images/pass_show.png" onClick={() => setShowConfirmPass("text")} />
+                </label>
+                {getWrongPassword(userPassword, confirmUserPassword)}
+                <button className="form-elem__button" onClick={() => checkPass(userPassword, confirmUserPassword) && addUser(userName, userEmail, confirmUserPassword)}>Sign up</button>
                 <span className="form-elem__footerSpan">Already have an account? &nbsp;
                     <Link to={"/login"}>
                         Sign in!
--- src/tools/users.ts
+++ src/tools/users.ts
@@ -1,9 +1,19 @@
-type User = {
-    name: string | null;
+const DOMAIN = "https://studapi.teachmeskills.by"
+const REGUSER = "/auth/users/"
+
+export type User = {
+    username: string | null;
     email: string | null;
     password: string | null;
 }
 
-export const onRegUser = async ({ name, email, password }: User) => {
-    
+export const regUser = async (user: User) => {
+    const regURL = new URL(DOMAIN + REGUSER)
+    const response = await fetch(regURL, {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: { "Content-Type": "application/json" }
+    })
+    const result = await response.json()
+    return result
 }
