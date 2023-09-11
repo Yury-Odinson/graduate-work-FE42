e628d23ce59fd27e30924f2a00015c2f06ec2c8c
yury-odinson <yury.odinson@gmail.com>
2023-09-11 17:01:58 +0300

    refactor

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -1,12 +1,10 @@
 export const ResetPassword = () => {
     return (
-        <>
-            <div className="form">
-                <p className="form__title">Reset password</p>
-                <span className="form-elem__span">Email</span>
-                <input type="text" className="form__input" placeholder="Your email"></input>
-                <button className="form-elem__button">Reset</button>
-            </div >
-        </>
+        <div className="form">
+            <p className="form__title">Reset password</p>
+            <span className="form-elem__span">Email</span>
+            <input type="text" className="form__input" placeholder="Your email"></input>
+            <button className="form-elem__button">Reset</button>
+        </div >
     )
 }
