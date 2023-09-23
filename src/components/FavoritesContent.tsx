5a9e7cf2555fb32eeb761d30ceb09e1e66864694
Yury <yury.odinson@gmail.com>
2023-09-23 08:15:23 +0300

    fix(favorites): remove endless cycle

--- src/components/FavoritesContent.tsx
+++ src/components/FavoritesContent.tsx
@@ -9,7 +9,7 @@ export const FavoritesContent = () => {
 
     useEffect(() => {
         getFavoritesMovies(JSON.parse(localStorage.getItem("idMovies") || "[]")).then((movie) => setMovies(movie))
-    }, [movies])
+    }, [])
 
     return (
         <div>
